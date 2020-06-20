f19886461e16c8c1fb46ca75254ae4d25c7de8a9
EsunR <641411169@qq.com>
2020-06-20 17:17:11 +0800

    fix: remove store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import "../node_modules/@mdi/font/css/materialdesignicons.min.css"
 import "./assets/mobile-event"
 import axios from 'axios'
@@ -16,6 +15,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
