770cf672fa6913c249691f6ecfb7731cb44ba34e
EsunR <641411169@qq.com>
2022-04-13 13:21:13 +0800

    fix: syntax lint

--- src/plugin/vue-touch/lib/vue-touch.d.ts
+++ src/plugin/vue-touch/lib/vue-touch.d.ts
@@ -1,5 +1,6 @@
 import { DirectiveBinding } from "vue";
 
 export default class VueTouch {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(el: any, binding: DirectiveBinding<any>, actionType: string);
 }
