bccb99415e69c2eb6114d0ef40032a9665e2893e
EsunR <641411169@qq.com>
2022-09-22 16:09:24 +0800

    build: 编译后的文件自动生成静态的gzip压缩文件

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default () => {
   return defineConfig({
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue(), eslintPlugin(), viteCompression()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src/"),
